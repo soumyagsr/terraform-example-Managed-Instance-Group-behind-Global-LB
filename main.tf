308ccfab5e564409ce1432bf2307d46f682d5aee
soumyagsr <34036848+soumyagsr@users.noreply.github.com>
2019-08-01 09:55:48 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,214 @@
+provider "google" {
+  project = "${var.project_name}"
+}
+
+provider "google-beta" {
+  project = "${var.project_name}"
+}
+
+
+# Create a new VPC network
+resource "google_compute_network" "tf-test-nw" {
+  name                    = "tf-vpc"
+  auto_create_subnetworks = "false"     # Will create subnets only in required regions
+  #routing_mode            = "GLOBAL"
+}
+
+# Create subnet in region 1
+resource "google_compute_subnetwork" "tf-subnet" {
+  name          = "tf-subnet"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "${var.region1}"
+  depends_on    = [google_compute_network.tf-test-nw]
+  network       = "${google_compute_network.tf-test-nw.self_link}"
+}
+
+# Create a managed instance group template
+resource "google_compute_instance_template" "default-mig-template" {
+  provider                = "google"
+  name                    = "tf-mig-template"
+  description             = "Template used to create the instances within the managed instance group."
+  instance_description    = "description assigned to instances"
+  machine_type            = "n1-standard-1"
+  tags                    = ["http-tag"]    # Tag(s) specified here must match the target_tags in the firewall rule
+  can_ip_forward          = false
+  depends_on              = [google_compute_network.tf-test-nw, google_compute_subnetwork.tf-subnet]
+  region                  = "${var.region1}"
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Hello from Terraform on Google Cloud!</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image = "debian-cloud/debian-9"
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    network    = "tf-vpc"
+    subnetwork = "tf-subnet"
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+  service_account {
+    scopes     = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+
+}
+
+
+# Create an HTTP health check
+resource "google_compute_health_check" "health-check" {
+  provider            = "google"
+  name                = "tf-health-check"
+  check_interval_sec  = 10
+  timeout_sec         = 10
+  healthy_threshold   = 2
+  unhealthy_threshold = 5             # 5 seconds
+  http_health_check {
+  }
+}
+
+# Create 1st regional managed instance group using the template defined above
+resource "google_compute_region_instance_group_manager" "mig-mgr1" {
+  provider                  = "google-beta"
+  name                      = "tf-mig-mgr1"
+  base_instance_name        = "tf-mig1"
+  version {
+    name               = "ver1"
+    instance_template  = "${google_compute_instance_template.default-mig-template.self_link}"
+  }
+  region                    = "${var.region1}"
+  distribution_policy_zones = ["${var.region1_zone1}", "${var.region1_zone2}", "${var.region1_zone3}"]
+  depends_on                = [google_compute_health_check.health-check, google_compute_instance_template.default-mig-template]
+  named_port {
+    name = "http"
+    port = "80"
+  }
+  auto_healing_policies {
+    health_check      = "${google_compute_health_check.health-check.self_link}"
+    initial_delay_sec = 300
+  }
+}
+
+
+# Create an autoscaler associated with the 1st managed instance group
+resource "google_compute_region_autoscaler" "autoscaler1" {
+  provider = "google-beta"
+  name   = "my-region-autoscaler1"
+  region = "${var.region1}"
+  target = "${google_compute_region_instance_group_manager.mig-mgr1.self_link}"
+  autoscaling_policy {
+    max_replicas    = 5
+    min_replicas    = 2
+    cooldown_period = 60
+    cpu_utilization {
+      target = 0.5
+    }
+  }
+}
+
+
+# Create a backend service pointing to the managed instance group defined above.
+# The backend service definition is where the IAP provisioning happens.
+# The OAuth2 client id and secret cannot be generated in Terrafom, you have to use the console.
+# In the console, go to API& Services -> Credentials to create the client id and client secret and copy them in the code below.
+
+resource "google_compute_backend_service" "default" {
+  provider      = "google"
+  name          = "tf-backend-svc-default"
+  #enable_cdn    = true
+  timeout_sec   = 3600
+  health_checks = ["${google_compute_health_check.health-check.self_link}"]
+  backend {
+    group = "${google_compute_region_instance_group_manager.mig-mgr1.instance_group}"
+  }
+
+  # Open bug on applying multiple backends: https://github.com/terraform-providers/terraform-provider-google/issues/3937
+  # As of now, you have to create two different backends
+  # backend {
+  #  group = "${google_compute_region_instance_group_manager.mig-mgr2.instance_group}"
+  # }
+}
+
+
+# Define the url map - use the path matcher to point to different backend services
+# Example of content based load balancing to different backends is contained in the repo listed in References
+resource "google_compute_url_map" "default" {
+  provider = "google"
+  name = "tf-test-url-map"
+  default_service = "${google_compute_backend_service.default.self_link}"
+  host_rule {
+    hosts = ["*"]
+    path_matcher = "tf-allpaths"
+  }
+  path_matcher {
+    name = "tf-allpaths"
+    default_service = "${google_compute_backend_service.default.self_link}"
+  }
+}
+
+
+# Define the HTTP(S) proxy where the forwarding rule forwards requests
+resource "google_compute_target_http_proxy" "http-lb-proxy" {
+  provider = "google"
+  name = "tf-http-lb-proxy"
+  url_map = "${google_compute_url_map.default.self_link}"
+}
+
+
+# Create a static global address (single anycast IP)
+resource "google_compute_global_address" "external-address" {
+  provider = "google"
+  name = "tf-external-address"
+}
+
+
+# Define the load balancer forwarding rule that sends traffic to the proxy defined above
+resource "google_compute_global_forwarding_rule" "default" {
+  provider    = "google"
+  name        = "tf-http-gfr"
+  target      = "${google_compute_target_http_proxy.http-lb-proxy.self_link}"
+  ip_address  = "${google_compute_global_address.external-address.address}"
+  port_range  = "80"
+}
+
+# Define firewall rule to allow traffic from the load balancer and health check
+resource "google_compute_firewall" "default" {
+  provider      = "google"
+  name          = "tf-test-firewall-allow-internal-only"
+  #network       = "{google_compute_network.tf-test-nw.self_link}"
+  network       = "tf-vpc"
+  depends_on    = [google_compute_network.tf-test-nw]
+  allow {
+    protocol    = "tcp"
+    ports       = ["80"]
+  }
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+  target_tags   = ["http-tag"]
+}
+
+
+/*
+# This block is optional - add it if you need SSH access to the VMs
+# Define firewall rule to allow SSH traffic
+resource "google_compute_firewall" "ssh" {
+  provider      = "google"
+  name          = "tf-test-firewall-allow-ssh"
+  network       = "tf-vpc"
+  #network       = "{google_compute_network.tf-test-nw.self_link}"
+  depends_on    = [google_compute_network.tf-test-nw]
+  allow {
+    protocol    = "tcp"
+    ports       = ["22"]
+  }
+  target_tags   = ["http-tag"]
+}
+
+*/
